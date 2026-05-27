6959b024a1ddfdcc81246d8524e0bea5975fcdae
Brayden Wayman <bwayman@byupathway.edu>
2026-05-27 11:39:35 -0600

    week 4 assignment

--- routes/auth.js
+++ routes/auth.js
@@ -2,41 +2,89 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const mongodb = require('../data/database');
+
 const router = express.Router();
 
-const User = require('../models/user');
 
-// Register a new user
+// REGISTER
 router.post('/register', async (req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const user = new User({
-            username: req.body.username,
-            password: hashedPassword
-        });
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  try {
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const user = {
+      username: req.body.username,
+      password: hashedPassword
+    };
+
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .insertOne(user);
+
+    res.status(201).json({
+      message: 'User created',
+      userId: response.insertedId
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
-// Login a user
+
+// LOGIN
 router.post('/login', async (req, res) => {
-    try {
-        const user = await User.findOne({ username: req.body.username });
-        if (!user) {
-            return res.status(400).json({ message: 'Invalid username or password' });
-        }
-        const validPassword = await bcrypt.compare(req.body.password, user.password);
-        if (!validPassword) {
-            return res.status(401).json({ message: 'Invalid username or password' });
-        }
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
+  try {
+
+    const user = await mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .findOne({
+        username: req.body.username
+      });
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid username'
+      });
     }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json({
+        message: 'Invalid password'
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        username: user.username
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '1h'
+      }
+    );
+
+    res.status(200).json({
+      token
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
 module.exports = router;
